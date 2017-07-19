88bdb04824220febbad09dcdd2fdfbbc31c866fe
hkamran <hkamran490@gmail.com>
2017-07-19 12:15:58 -0400

    added nand gate

---
+++ src/test/java/com/hkamran/ai/tests/gates/NANDGateTest.java
@@ -0,0 +1,117 @@
+package com.hkamran.ai.tests.gates;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.hkamran.ai.Activations;
+import com.hkamran.ai.BackPropNetwork;
+import com.hkamran.ai.LayerBuilder;
+import com.hkamran.ai.NetworkBuilder;
+import com.hkamran.ai.NetworkBuilder.NetworkType;
+
+public class NANDGateTest {
+	private static final double MIN_ERROR = 0.0001;
+	private static final int TRAINING_LIMIT = 250000;
+	static BackPropNetwork network;
+	
+	@BeforeClass
+	public static void xorTest() throws InterruptedException {
+		network = 
+				(BackPropNetwork) NetworkBuilder
+				.create(NetworkType.BACKPROP)
+				.setLabel("XOR GATE")
+				.setInputLayer(
+						LayerBuilder
+						.create()
+						.addNodes(2, Activations.sigmoid))
+				.addHiddenLayer(
+						LayerBuilder
+						.create()
+						.addNodes(4, Activations.sigmoid))
+				.setOutputLayer(
+						LayerBuilder
+						.create()
+						.addNodes(1, Activations.sigmoid)
+						)
+				.withBiasNode()
+				.createAllConnections()
+				.build();		
+		
+		network.setTrainingDataSet(
+				new double[][] {{1, 1}, {1, 0}, {0, 1}, {0, 0}}, 
+				new double[][] {{0}, {1}, {1}, {1}});
+		
+		train(network);
+	}
+
+	private static void train(BackPropNetwork network) {
+		int cycle = 0;
+		while (network.getTotalError() > MIN_ERROR && cycle < TRAINING_LIMIT) {
+			network.clear();
+			network.train(1);
+			if (cycle % 10000 == 0) System.out.println(cycle);
+			cycle++;
+		};
+		
+		if (cycle >= TRAINING_LIMIT) {
+			throw new RuntimeException("Unable to train network! " + network.getTotalError());
+		}
+	}
+	
+	@Test
+	public void inputOneAndOneExpectZero() {
+		network.clear();
+		network.setInput(new double[] {1,1});
+		network.calculate();
+		double[] expected = new double[] {0};
+		double[] actual = network.getOutput();
+		for (int i = 0; i < actual.length; i++) {
+			actual[i] = Math.round(actual[i]);
+		}
+		
+		Assert.assertArrayEquals(expected, actual, 0);
+	}
+
+	@Test
+	public void inputZeroAndZeroExpectOne() {
+		network.clear();
+		network.setInput(new double[] {0, 0});
+		network.calculate();
+		double[] expected = new double[] {1};
+		double[] actual = network.getOutput();
+		for (int i = 0; i < actual.length; i++) {
+			actual[i] = Math.round(actual[i]);
+		}
+		
+		Assert.assertArrayEquals(expected, actual, 0);
+	}	
+	
+	@Test
+	public void inputOneAndZeroExpectOne() {
+		network.clear();
+		network.setInput(new double[] {1, 0});
+		network.calculate();
+		double[] expected = new double[] {1};
+		double[] actual = network.getOutput();
+		for (int i = 0; i < actual.length; i++) {
+			actual[i] = Math.round(actual[i]);
+		}
+
+		Assert.assertArrayEquals(expected, actual, 0);
+	}		
+	
+	@Test
+	public void inputZeroAndOneExpectOne() {
+		network.clear();
+		network.setInput(new double[] {0, 1});
+		network.calculate();
+		double[] expected = new double[] {1};
+		double[] actual = network.getOutput();
+		for (int i = 0; i < actual.length; i++) {
+			actual[i] = Math.round(actual[i]);
+		}
+
+		Assert.assertArrayEquals(expected, actual, 0);
+	}	
+}
